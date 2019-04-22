b2070472eedf0b040977c15a453b332c39d8f4b3
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-04-22 20:37:01 +0800

    initial commit

---
+++ MyBaseLocalizationSDK/MyBaseLocalizationSDK.h
@@ -0,0 +1,19 @@
+//
+//  MyBaseLocalizationSDK.h
+//  MyBaseLocalizationSDK
+//
+//  Created by Lainel John Dela Cruz on 22/04/2019.
+//  Copyright © 2019 Lainel John Dela Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyBaseLocalizationSDK.
+FOUNDATION_EXPORT double MyBaseLocalizationSDKVersionNumber;
+
+//! Project version string for MyBaseLocalizationSDK.
+FOUNDATION_EXPORT const unsigned char MyBaseLocalizationSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyBaseLocalizationSDK/PublicHeader.h>
+
+
